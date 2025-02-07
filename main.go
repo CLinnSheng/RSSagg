ca01c4b86567c21e550a3be036ae4deea660320d
linnsheng <linnsheng2002@gmail.com>
2025-02-07 22:16:13 +0800

    protocol for client request to server

--- main.go
+++ main.go
@@ -2,8 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	godotenv.Load(".env")
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT IS NOT FOUND IN DONTENV")
+	}
+
+	// router object
+	router := chi.NewRouter()
+
+	// options to give the client how can they access the server
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://*", "https://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300}))
+
+	// server object
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server is running on port %s", portString)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(portString)
 }
